3c6bc08952d9a9f002bbc703bc0422b90bd14930
wonjonghong <wonhong1996@naver.com>
2025-04-03 17:05:37 +0900

    feat/models 앱의 색상들을 styles/app_theme.dart에 정립

--- lib/styles/app_theme.dart
+++ lib/styles/app_theme.dart
@@ -3,3 +3,4 @@ import 'dart:ui';
 const Color bottomBarColor = Color(0xFF4D81F0);
 const Color appBarColor = Color(0xFFEFEFEF);
 const Color addButtonColor = Color(0xFF55DAB4);
+const Color deleteButtonColor = Color(0xFFEF6755);
