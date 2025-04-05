944224ecc7eb507f2bfcca8daca0fd3ed595d059
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-04-05 22:06:00 +0900

    Feat: 상품 상세 스크롤 가능하게 변경

--- lib/pages/item_detail_page.dart
+++ lib/pages/item_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import '../components/custom_app_bar.dart';
@@ -19,78 +18,69 @@ class ItemDetailPage extends ConsumerWidget {
     final items = ref.watch(itemListProvider);
     final item = items.firstWhere(
       (item) => item.id == itemId,
-      orElse:
-          () => Item(
-            id: '',
-            name: '아이템 없음',
-            company_name: "이름 없음",
-            price: 0.0,
-            description: '해당 아이템을 찾을 수 없습니다.',
-            item_type: ItemType.None,
-          ),
+      orElse: () => Item(
+        id: '',
+        name: '아이템 없음',
+        company_name: "이름 없음",
+        price: 0.0,
+        description: '해당 아이템을 찾을 수 없습니다.',
+        item_type: ItemType.None,
+      ),
     );
 
     return Scaffold(
       appBar: CustomAppBar(title: item.name),
-      body: Container(
-        padding: EdgeInsets.all(16),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             item.imageFile != null
                 ? Image.file(
-                  item.imageFile!,
-                  height: 250,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                )
+                    item.imageFile!,
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  )
                 : Container(
-                  height: 200,
-                  color: Colors.grey[300],
-                  child: const Center(child: Text('이미지 없음')),
-                ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(item.name, style: TextStyle(fontSize: 30)),
-                    Text(
-                      '${NumberFormat("#,###", "ko_KR").format(item.price)}원',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.red[800],
-                      ),
-                    ),
-                  ],
-                ),
-                Spacer(),
-                Container(
-                  padding: EdgeInsets.all(3),
-                  decoration: BoxDecoration(
-                    border: Border.all()
+                    height: 200,
+                    color: Colors.grey[300],
+                    child: const Center(child: Text('이미지 없음')),
                   ),
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(item.name, style: const TextStyle(fontSize: 25)),
+                      const SizedBox(height: 4),
+                      Text(
+                        '${NumberFormat("#,###", "ko_KR").format(item.price)}원',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
                   child: Row(
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                        ),
-                        padding: EdgeInsets.symmetric(horizontal: 30),
-                        child: Text('0',
-                          style: TextStyle(
-                            fontSize: 25
-                          ),
-                        ),
-                      ),
+                      const Text('0', style: TextStyle(fontSize: 20)),
                       Column(
-                        children: [
-                          Icon(Icons.keyboard_arrow_up,
-                            size: 30,
-                          ),
-                          Icon(Icons.keyboard_arrow_down,
-                            size: 30,
-                          ),
+                        children: const [
+                          Icon(Icons.keyboard_arrow_up, size: 25),
+                          Icon(Icons.keyboard_arrow_down, size: 25),
                         ],
                       ),
                     ],
@@ -98,32 +88,35 @@ class ItemDetailPage extends ConsumerWidget {
                 ),
               ],
             ),
-
-            Spacer(),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  _showQuantityDialog(context, item, ref);
-                  //_showQuantityDialog 거치지 않고 바로 장바구니에 추가
-                },
-                child: Text(
-                  '장바니에 추가',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  padding: EdgeInsets.symmetric(vertical: 16),
-                ),
-              ),
+            const SizedBox(height: 20),
+            Text(
+              item.description,
+              style: const TextStyle(fontSize: 16),
             ),
           ],
         ),
       ),
+      bottomNavigationBar: Container(
+          padding: EdgeInsets.all(16),
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: () {
+              _showQuantityDialog(context, item, ref);
+            },
+            child: Text(
+              "구매하기",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              padding: EdgeInsets.symmetric(vertical: 16),
+            ),
+          ),
+        ),
     );
   }
 
@@ -135,12 +128,12 @@ class ItemDetailPage extends ConsumerWidget {
       context: context,
       builder: (context) {
         return CupertinoAlertDialog(
-          title: Text('장바구니에 추가할 수량을 입력하세요'),
+          title: const Text('장바구니에 추가할 수량을 입력하세요'),
           content: CupertinoTextField(
             controller: quantityController,
             keyboardType: TextInputType.number,
             placeholder: '수량 입력',
-            padding: EdgeInsets.all(12),
+            padding: const EdgeInsets.all(12),
           ),
           actions: [
             CupertinoDialogAction(
@@ -148,7 +141,7 @@ class ItemDetailPage extends ConsumerWidget {
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('취소'),
+              child: const Text('취소'),
             ),
             CupertinoDialogAction(
               isDefaultAction: true,
@@ -158,7 +151,7 @@ class ItemDetailPage extends ConsumerWidget {
                 Navigator.of(context).pop(); // 다이얼로그 닫기
                 _addCartDialog(context); // 장바구니 이동 확인 다이얼로그 띄우기
               },
-              child: Text('추가'),
+              child: const Text('추가'),
             ),
           ],
         );
@@ -177,7 +170,7 @@ class ItemDetailPage extends ConsumerWidget {
             borderRadius: BorderRadius.circular(12),
           ),
           title: Column(
-            children: [
+            children: const [
               Icon(Icons.shopping_cart, color: Colors.green, size: 60),
               SizedBox(height: 10),
               Text(
@@ -186,7 +179,7 @@ class ItemDetailPage extends ConsumerWidget {
               ),
             ],
           ),
-          content: Text(
+          content: const Text(
             "장바구니에 해당 품목이 담겼습니다.",
             style: TextStyle(fontSize: 14, color: Colors.grey),
           ),
@@ -195,7 +188,7 @@ class ItemDetailPage extends ConsumerWidget {
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: Text("확인", style: TextStyle(color: Colors.black)),
+              child: const Text("확인", style: TextStyle(color: Colors.black)),
             ),
             TextButton(
               onPressed: () {
@@ -203,10 +196,10 @@ class ItemDetailPage extends ConsumerWidget {
                 Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (context) => const CartItemPage(),
-                  ), // 장바구니로 이동
+                  ),
                 );
               },
-              child: Text("장바구니 이동", style: TextStyle(color: Colors.blue)),
+              child: const Text("장바구니 이동", style: TextStyle(color: Colors.blue)),
             ),
           ],
         );
